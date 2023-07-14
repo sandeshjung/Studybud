e98507d2c510d05121e5d1b3b46d5715ab2a6431
Sandesh Jung Kunwar <sandeshjung@gmail.com>
2023-07-14 21:35:46 +0545

    Edit User

--- base/views.py
+++ base/views.py
@@ -7,7 +7,7 @@ from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic, Message
-from .forms import RoomForm
+from .forms import RoomForm, UserForm
 # Create your views here.
 
 # rooms = [
@@ -175,9 +175,22 @@ def deleteMessage(request, pk):
 
     if request.user != message.user:
         return HttpResponse('You are not allowed to edit this room')
-    
+
     if request.method == 'POST':
         message.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj' : message})
 
+
+@login_required(login_url='login')
+def updateUser(request):
+    user = request.user
+    form = UserForm(instance=user)
+
+    if request.method == 'POST':
+        form = UserForm(request.POST, instance=user)
+        if form.is_valid():
+            form.save()
+            return redirect('user-profile', pk=user.id)
+
+    return render(request, 'base/update_user.html', {'form' : form})
