5a27704fad5a5e421d821979b5e4354a69bc1c34
Sandesh Jung Kunwar <sandeshjung@gmail.com>
2023-07-14 20:32:21 +0545

    Applying frontend theme

--- base/urls.py
+++ base/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('login/', views.loginPage, name='login-page'),
+    path('login/', views.loginPage, name='login'),
     path('logout/', views.logoutUser, name='logout'),
     path('register/', views.registerUser, name='register'),
     path('', views.home, name='home'),
